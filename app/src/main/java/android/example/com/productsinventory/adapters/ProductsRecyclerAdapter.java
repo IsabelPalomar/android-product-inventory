78f73f56fb17d2239f9dd84811c9c35cee10b15f
Isabel Palomar <isabel.palomarb@gmail.com>
2016-07-04 02:07:08 -0400

    NumberPicker to track sale

--- app/src/main/java/android/example/com/productsinventory/adapters/ProductsRecyclerAdapter.java
+++ app/src/main/java/android/example/com/productsinventory/adapters/ProductsRecyclerAdapter.java
@@ -1,6 +1,9 @@
 package android.example.com.productsinventory.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.example.com.productsinventory.MainActivity;
 import android.example.com.productsinventory.R;
 import android.example.com.productsinventory.data.Product;
 import android.support.v7.widget.CardView;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.util.List;
@@ -59,6 +63,42 @@ public class ProductsRecyclerAdapter extends RecyclerView.Adapter<ProductsRecycl
 
             }
 
+        });
+
+        holder.sellItem.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(context);
+
+                alert.setTitle("Select the value: ");
+
+                NumberPicker np = new NumberPicker(context);
+                String[] nums = new String[100];
+                for(int i=0; i<nums.length; i++)
+                    nums[i] = Integer.toString(i);
+
+                np.setMinValue(1);
+                np.setMaxValue(nums.length-1);
+                np.setWrapSelectorWheel(false);
+                np.setDisplayedValues(nums);
+                np.setValue(50);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Do something with value!
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Cancel.
+                    }
+                });
+
+                alert.setView(np);
+                alert.show();
+            }
         });
 
     }
